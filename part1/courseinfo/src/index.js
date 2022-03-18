872720959bf4cd5cef93a0eb541d78b86f687293
Cristian Marin <cristiancamilomarinflorez@gmail.com>
2022-03-17 20:51:54 -0500

    Ajustes

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -34,27 +34,31 @@ const Total = (props) => (
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =  [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const total= course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
 
   return (
     <div>
-      <Header course={course} />
-      <Content partes = {parts} />
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Header course={course.name} />
+      <Content partes = {course.parts} />
+      <Total total={total} />
     </div>
   )
 }
